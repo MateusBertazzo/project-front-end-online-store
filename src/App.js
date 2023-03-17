c4969cf1be51d168f1befd2a6dc9f2116a85cbc9
YoshuaG <yoshua.gar@gmail.com>
2023-03-17 17:15:04 -0300

    req2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './page/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+
+      <Switch>
+        <Route exact path="/" component={ Home } />
+      </Switch>
+
+    );
+  }
 }
 
 export default App;
---
+++ src/page/Home.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+
+      <div>
+        <form action="">
+          <input />
+          <p data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+
+        </form>
+      </div>
+
+    );
+  }
+}
+
+export default Home;

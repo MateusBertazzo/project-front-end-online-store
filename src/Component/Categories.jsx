0deb1c61c4e2642023e1bece051ef6bf63d39fd2
YoshuaG <yoshua.gar@gmail.com>
2023-03-17 21:03:32 -0300

    req6

--- src/Component/Categories.jsx
+++ src/Component/Categories.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
+import Products from './Products';
 
 class Categories extends Component {
   state = {
     categories: [],
+    productsByCat: [],
   };
 
   componentDidMount() {
@@ -13,20 +15,43 @@ class Categories extends Component {
     });
   }
 
+  handleClick = async ({ target }) => {
+    const { id } = target;
+    const getProduct = await getProductsFromCategoryAndQuery(id);
+    this.setState({
+      productsByCat: getProduct.results,
+    });
+  };
+
   render() {
-    const { categories } = this.state;
+    const { categories, productsByCat } = this.state;
     return (
       <div>
         <h2>categorias</h2>
         <ul>
           {categories.map((categorie) => (
             <li key={ categorie.id }>
-              <button data-testid="category" type="button">
+              <button
+                data-testid="category"
+                type="button"
+                onClick={ this.handleClick }
+                id={ categorie.id }
+              >
                 {categorie.name}
               </button>
             </li>
           ))}
         </ul>
+        {productsByCat.map((product) => (
+          <Products
+            data-testid="product"
+            key={ product.id }
+            title={ product.title }
+            thumbnail={ product.thumbnail }
+            price={ product.price }
+            id={ product.id }
+          />
+        ))}
       </div>
 
     );
